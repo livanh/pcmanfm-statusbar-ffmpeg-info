7cbc99963804051e12e344bdf8b5b57e4f5e6778
Livanh <livanh@protonmail.com>
2018-12-09 12:29:50 +0100

    Changed email address

--- pcmanfm-statusbar-ffmpeg-info.c
+++ pcmanfm-statusbar-ffmpeg-info.c
@@ -1,7 +1,7 @@
 /*
  * pcmanfm-statusbar-ffmpeg-info.c
  *
- * Copyright (C) 2017 Livanh <livanh@bulletmail.org>
+ * Copyright (C) 2017 Livanh <livanh@protonmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
